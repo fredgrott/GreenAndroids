39dfd6ffea8c9a30cc2cd1f37f3dc68aedd5ab68
Fred Grott <fred.grott@gmail.com>
2016-09-27 18:50:34 -0500

    cannot do whiteandroid as one library module, will have to redo it as separate library project with multimiple modules..but that is for later need to move on the app demo code

---
+++ whiteandroid/src/main/java/com/github/shareme/greenandroids/whiteandroid/actions/ActionsModule.java
@@ -0,0 +1,89 @@
+package com.github.shareme.greenandroids.whiteandroid.actions;
+
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.github.shareme.greenandroids.whiteandroid.R;
+import com.github.shareme.greenandroids.whiteandroid.base.DebugModule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ActionsModule implements DebugModule {
+    private final List<Action> actions = new ArrayList<>();
+
+    public ActionsModule(Action... actions) {
+        if (actions != null) {
+            for (int i = 0; i < actions.length; i++) {
+                this.actions.add(actions[i]);
+            }
+        }
+    }
+
+    @NonNull @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup parent) {
+        LinearLayout view = (LinearLayout) inflater.inflate(R.layout.debug_drawer_module_actions, parent, false);
+
+        if (actions.isEmpty()) {
+            TextView noActionsLabel = new TextView(parent.getContext());
+            noActionsLabel.setTextColor(ContextCompat.getColor(parent.getContext(), R.color.white));
+            noActionsLabel.setText("No actions added");
+            view.addView(noActionsLabel);
+        } else {
+            for (Action action : actions) {
+                view.addView(action.getView(view));
+            }
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onOpened() {
+        for (Action action : actions) {
+            action.onOpened();
+        }
+    }
+
+    @Override
+    public void onClosed() {
+        for (Action action : actions) {
+            action.onClosed();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        for (Action action : actions) {
+            action.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        for (Action action : actions) {
+            action.onPause();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        for (Action action : actions) {
+            action.onStart();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        for (Action action : actions) {
+            action.onStop();
+        }
+    }
+}
